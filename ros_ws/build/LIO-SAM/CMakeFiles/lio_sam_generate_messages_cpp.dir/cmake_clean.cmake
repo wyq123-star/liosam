732fb6e5e25ec4f79614ff36af241399851572f3
yutouxiaozhuang <3186831775@qq.com>
2025-08-30 15:30:16 +0800

    Ppa install (#1)
    * 加入镜像构建脚本和compose文件
    
    * 成功在容器中安装gtsam
    
    * 修补容器启动文件
    
    * 不传入数据包

---
+++ ros_ws/build/LIO-SAM/CMakeFiles/lio_sam_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lio_sam_generate_messages_cpp"
+  "/home/yutou/lio_sam/ros_ws/devel/include/lio_sam/cloud_info.h"
+  "/home/yutou/lio_sam/ros_ws/devel/include/lio_sam/save_map.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lio_sam_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_ws/build/LIO-SAM/CMakeFiles/lio_sam_generate_messages_eus.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lio_sam_generate_messages_eus"
+  "/home/yutou/lio_sam/ros_ws/devel/share/roseus/ros/lio_sam/msg/cloud_info.l"
+  "/home/yutou/lio_sam/ros_ws/devel/share/roseus/ros/lio_sam/srv/save_map.l"
+  "/home/yutou/lio_sam/ros_ws/devel/share/roseus/ros/lio_sam/manifest.l"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lio_sam_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
